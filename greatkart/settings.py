b00f64eb1f1b46dd69d1b0a7c651228f75d7db90
Uddhav-singh <uddhav.singh.79@gmail.com>
2023-01-14 12:40:55 +0530

    store and carts functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processor.menu_links', #added this to use this function in any templates we want
+                'carts.context_processors.counter',
             ],
         },
     },
